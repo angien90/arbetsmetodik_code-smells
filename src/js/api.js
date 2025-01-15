c2662f3f1d68fb6fc6c3dd50d3936a7bcc1ca002
angien90 <angelicanylander@hotmail.com>
2025-01-15 16:41:51 +0100

    feat(error message): Add error message to show when API doesn't work

--- src/js/api.js
+++ src/js/api.js
@@ -10,8 +10,26 @@ export async function getPodcasts() {
         return data;
     } catch (error) {
         console.error('Något blev fel:', error);
+        displayErrorMessage("Vi kunde inte hämta podcastinformation just nu. Försök igen senare!");
         return null;
     }
+}
+
+/**
+ * Funktion för felmeddelande när podcastinformation inte kan visas
+ * Dialogen stängs efter 10 sekunder
+ * @param {*} message  - Meddelande som visas för användaren
+ */
+function displayErrorMessage(message) {
+    const errorMessage = document.createElement('div');
+    errorMessage.classList.add('error-message');
+    errorMessage.textContent = message;
+
+    document.body.appendChild(errorMessage);
+
+    setTimeout(() => {
+        errorMessage.remove();
+    }, 10000);
 }
 
 export default getPodcasts;
