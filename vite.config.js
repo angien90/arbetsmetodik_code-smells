76b2facd7852dfe8ecd50642471aa4aad301bf30
angien90 <angelicanylander@hotmail.com>
2025-02-16 11:41:55 +0100

    fix(changes link to css)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/arbetsmetodik_code-smells/',
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify('https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false')
-  }
 });
